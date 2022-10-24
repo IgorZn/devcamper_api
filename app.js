781b27e7d6ec55b16f1a36f7877ae43abb760db1
Igor Znamensky <igor.znamensky@gmail.com>
2022-10-24 23:00:19 +0300

    Chapter: 03 Done: 3. Creating Routes & Responses In Express Next: 4. Using The Express Router

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Routers file
+const bootcamps = require('./routes/bootcamps.route');
+var indexRouter = require('./routes/index.route');
+var usersRouter = require('./routes/users.route');
 
 var app = express();
 
@@ -19,6 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
---
+++ routes/bootcamps.route.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const {route} = require("express/lib/router");
+const router = express.Router();
+
+router.route('/')
+    /* GET bootcamps */
+    .get(function (req, res, next) {
+        res.status(200).json({success: false, msg: 'Show all bootcamps'});
+    })
+
+
+router.route('/:id')
+    /* GET bootcamps */
+    .get(function (req, res, next) {
+        res.status(200).json({success: false, msg: 'Show all bootcamps'});
+    })
+    /* POST bootcamps */
+    .post(function (req, res, next) {
+        res.status(200).json({success: false, msg: 'Create new bootcamps'});
+    })
+    /* PUT bootcamps */
+    .put(function (req, res, next) {
+        res.status(200).json({success: false, msg: `Display bootcamps ${req.params.id}`});
+    })
+    /* DELETE bootcamps */
+    .delete(function (req, res, next) {
+        res.status(200).json({success: false, msg: `Delete bootcamps ${req.params.id}`});
+    })
+
+
+
+module.exports = router;
