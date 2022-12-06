8e7b8344c305d12beee5e54067d3f5712ee9d689
Igor Znamensky <igor.znamensky@glera-games.com>
2022-12-06 10:39:37 +0300

    Chapter: 11 Done: Chapter: 10, 4. Rate Limiting, HPP & CORS Next: Chapter: 11

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ const dotenv = require("dotenv");
 const connDB = require("./conf/db");
 const fileUpload = require("express-fileupload");
 
+const cors = require('cors')
+
 const mongoSanitize = require('express-mongo-sanitize');
 const bodyParser = require('body-parser');
 
 const helmet = require("helmet");
-const xss = require('xss-clean')
+const xss = require('xss-clean');
+
+/* Basic rate-limiting middleware for Express */
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
 // Load env consts
 dotenv.config({ path: './conf/config.env' });
@@ -20,8 +26,24 @@ dotenv.config({ path: './conf/config.env' });
 // Connect to DB
 connDB()
 
-
+/* Init Express server */
 const app = express();
+
+/* Enable CORS with various options */
+app.use(cors())
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
+
+/* Protect against HTTP Parameter Pollution attacks */
+app.use(hpp())
 
 /* Mongo Sanitize */
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -52,9 +74,9 @@ app.use(fileUpload({
 }));
 
 
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 // Dev logger
 if (process.env.NODE_ENV.startsWith('dev')) {
